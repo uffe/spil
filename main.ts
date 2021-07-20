778b49977a71d3d73b9740db64594e5b39dfc217
Uffe Koch <uffe@detgodefirma.dk>
2021-07-20 22:47:48 +0200

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,18 +2,29 @@ input.onButtonPressed(Button.A, function () {
     plads += -1
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    skud = game.createSprite(plads, 4)
+    skud.set(LedSpriteProperty.Blink, 50)
+    for (let indeks = 0; indeks <= 4; indeks++) {
+        basic.pause(100)
+        skud.change(LedSpriteProperty.Y, -1)
+    }
+    if (skud.isTouching(fjende)) {
+        fjende.delete()
+    }
+    skud.delete()
 })
 input.onButtonPressed(Button.B, function () {
     plads += 1
 })
-let plads = 2
+let skud: game.LedSprite = null
+let fjende: game.LedSprite = null
+let plads = 0
+plads = 2
 let fjende_plads = 2
-let fjende = game.createSprite(fjende_plads, 0)
+fjende = game.createSprite(fjende_plads, 0)
 let fjende_retning = 1
 let helt = game.createSprite(2, 4)
-fjende.set(LedSpriteProperty.Blink, 300)
-loops.everyInterval(500, function () {
+loops.everyInterval(1000, function () {
     if (fjende_plads == 4) {
         fjende_retning = -1
     }
